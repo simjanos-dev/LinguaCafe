e37cbde91d1eecbb11ec27a797e7ebc84789aa7a
simjanos-dev <simjanos.dev@gmail.com>
2024-01-12 00:22:24 +0100

    Reverting image changes

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -12,35 +12,13 @@ class ImageController extends Controller
         $this->middleware('auth');
     }
 
-    private function getImage($basePath, $name, $defaultImagePath)
-    {
-        $extensions = ['svg', 'jpg', 'png'];
-
-        foreach ($extensions as $extension) {
-            $path = $basePath . $name . '.' . $extension;
-
-            if (Storage::exists($path)) {
-                return Storage::get($path);
-            }
-        }
-
-        // If none of the specified extensions exist, return the content of the default image
-        return Storage::get($defaultImagePath);
-    }
-
     public function getFlagImage($name)
     {
-        $basePath = '/images/flags/';
-        $defaultImagePath = '/images/flags/default-flag.svg';
-
-        return $this->getImage($basePath, $name, $defaultImagePath);
+        return Storage::get('/images/flags/' . $name . '.png');
     }
 
     public function getBookImage($name)
     {
-        $basePath = '/images/book_images/';
-        $defaultImagePath = '/images/book_images/default.jpg';
-
-        return $this->getImage($basePath, $name, $defaultImagePath);
+        return Storage::get('/images/book_images/' . $name);
     }
 }
