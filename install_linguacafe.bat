ea682bee1c1446820e2ca363333873b7dfc39264
Tlaborde <thomas.laborde@gmail.com>
2024-02-09 09:53:45 +0900

    check docker engine, default path, no browser open

--- install_linguacafe.bat
+++ install_linguacafe.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal
+setlocal EnableDelayedExpansion
 
 REM Check if git is installed
 git --version >nul 2>&1
@@ -17,29 +17,46 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-REM Prompt user for installation directory
-set /p install_dir="Enter installation directory (press Enter for current directory '%cd%\'): "
-if "%install_dir%"=="" set "install_dir=%cd%"
+REM Check if Docker Engine is running
+docker info > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Docker Engine is not running. Please start Docker Desktop and try again.
+    timeout /t 10 >nul
+    exit /b 1
+) 
 
-REM Clone repository or update if folder exists
-cd %install_dir%
+REM If the folder already exists, update and close
 if exist linguacafe (
+    echo Installation of LingaCafe detected, updating...
     cd linguacafe
     git pull
     docker-compose pull
     docker-compose up -d --force-recreate
-) else (
+    timeout /t 10 >nul
+    exit /b 0
+)
+
+REM Display suggested installation directory including "linguacafe" folder
+set "install_dir=%cd%\linguacafe"
+echo The installation directory will be: !install_dir!
+
+REM Ask user to confirm or cancel installation
+set /p confirm_installation=Do you want to proceed with the installation? (Y/N): 
+
+REM Check user's response
+if /i "!confirm_installation!"=="Y" (
+    echo Proceeding with installation...
+    REM Clone repository or update if folder exists
     git clone -b deploy https://github.com/simjanos-dev/LinguaCafe.git linguacafe
     cd linguacafe
     docker-compose up -d
+
+    echo Your LinguaCafe server has started, you can soon open it in your browser on this url: http://localhost:9191. Sometimes this can take a few minutes on slower computers.
+    set /p close="Press Enter to close this window."
+) else (
+    echo Installation canceled. Exiting...
+    exit /b 1
 )
-
-REM Wait for 20 seconds
-echo "Waiting 20 seconds before opening a browser to access LingaCafe at the url http://localhost:9191"
-timeout /t 20 >nul
-
-REM Open browser on localhost:9191
-start http://localhost:9191
 
 :end
 endlocal
