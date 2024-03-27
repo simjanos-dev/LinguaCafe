320a94e1ebeebc9730f75c0d53ef731c1b4898a0
sergiolaverde0 <sergiolaverde0@noreply.codeberg.org>
2024-03-26 21:14:34 -0500

    fixup! fix: container no longer needs to be restarted after installing a model

--- tools/tokenizer.py
+++ tools/tokenizer.py
@@ -481,7 +481,7 @@ model_url: dict[str, str] = {
     "japanese": "https://github.com/explosion/spacy-models/releases/download/ja_core_news_sm-3.7.0/ja_core_news_sm-3.7.0-py3-none-any.whl",
     "korean": "https://github.com/explosion/spacy-models/releases/download/ko_core_news_sm-3.7.0/ko_core_news_sm-3.7.0-py3-none-any.whl",
     "russian": "https://github.com/explosion/spacy-models/releases/download/ru_core_news_sm-3.7.0/ru_core_news_sm-3.7.0-py3-none-any.whl",
-    "ukranian": "https://github.com/explosion/spacy-models/releases/download/uk_core_news_sm-3.7.0/uk_core_news_sm-3.7.0-py3-none-any.whl",
+    "ukrainian": "https://github.com/explosion/spacy-models/releases/download/uk_core_news_sm-3.7.0/uk_core_news_sm-3.7.0-py3-none-any.whl",
     "chinese": "https://github.com/explosion/spacy-models/releases/download/zh_core_web_sm-3.7.0/zh_core_web_sm-3.7.0-py3-none-any.whl",
 }
 
@@ -489,7 +489,7 @@ model_name: dict[str, str] = {
     "ja-core-news-sm": "japanese",
     "ko-core-news-sm": "korean",
     "ru-core-news-sm": "russian",
-    "uk-core-news-sm": "ukranian",
+    "uk-core-news-sm": "ukrainian",
     "zh-core-web-sm": "chinese",
 }
 
