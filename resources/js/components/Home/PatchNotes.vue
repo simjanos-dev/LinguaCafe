8bc73b5ce734c5218586648d2a95568860ba3d80
simjanos-dev <simjanos.dev@gmail.com>
2024-04-27 20:33:36 +0200

    Update notes

--- resources/js/components/Home/PatchNotes.vue
+++ resources/js/components/Home/PatchNotes.vue
@@ -3,24 +3,6 @@
         <div class="subheader">
             Patch notes
         </div>
-        
-        <v-card outlined class="rounded-lg mb-8">
-            <v-card-title>v0.11.2 <v-spacer /> 2024.04.27.</v-card-title>
-            <v-card-text>
-                <b>Bug fixes:</b>
-                <ul>
-                    <li>
-                        Fixed annoying multi word text selection bug.
-                    </li>
-                    <li>
-                        Fixed furigana.
-                    </li>
-                    <li>
-                        Fixed plain text mode.
-                    </li>
-                </ul>
-            </v-card-text>
-        </v-card>
 
         <v-card outlined class="rounded-lg mb-8">
             <v-card-title>v0.12 <v-spacer /> 2024.??.??.</v-card-title>
@@ -86,6 +68,23 @@
             </v-card-text>
         </v-card>
 
+        <v-card outlined class="rounded-lg mb-8">
+            <v-card-title>v0.11.2 <v-spacer /> 2024.04.27.</v-card-title>
+            <v-card-text>
+                <b>Bug fixes:</b>
+                <ul>
+                    <li>
+                        Fixed annoying multi word text selection bug.
+                    </li>
+                    <li>
+                        Fixed furigana.
+                    </li>
+                    <li>
+                        Fixed plain text mode.
+                    </li>
+                </ul>
+            </v-card-text>
+        </v-card>
 
         <v-card outlined class="rounded-lg mb-8">
             <v-card-title>v0.11 <v-spacer /> 2024.04.25.</v-card-title>
