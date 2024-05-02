d8f8da17c6bb05cfded8def49b562606b3f27733
simjanos-dev <simjanos.dev@gmail.com>
2024-05-03 00:07:12 +0200

    Added web app support

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="manifest" href="./manifest.json"> 
+    
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
