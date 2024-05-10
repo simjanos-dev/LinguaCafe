dec0c27f23f83178711844d2ba78e95e3d5a652e
simjanos-dev <simjanos.dev@gmail.com>
2024-05-10 15:14:48 +0200

    Moved default font files from the storage to the public folder

--- config/filesystems.php
+++ config/filesystems.php
@@ -35,6 +35,11 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'default-files' => [
+            'driver' => 'local',
+            'root' => public_path('default'),
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
