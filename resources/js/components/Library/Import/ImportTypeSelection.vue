4f7d59a970c2197e712bb7ddf9da888c0e5f7588
simjanos-dev <simjanos.dev@gmail.com>
2024-05-15 17:23:35 +0200

    Disabled Thai import temporarily

--- resources/js/components/Library/Import/ImportTypeSelection.vue
+++ resources/js/components/Library/Import/ImportTypeSelection.vue
@@ -2,84 +2,97 @@
     <div>
         <label class="font-weight-bold">Select an import option</label>
         <div class="import-type-group flex-wrap">
-            <!--Plain text -->
-            <div class="import-type-button rounded-lg mx-2 mb-4" @click="selectImportType('plain-text')">
-                <div class="import-type-button-icon-box">
-                    <v-icon large>mdi-text-box</v-icon>
+            <template v-if="language == 'thai'">
+                <v-alert
+                    class="w-full"
+                    color="error"
+                    type="error"
+                    border="left"
+                >
+                    There was a problem with importing Thai texts, so I temporarily disabled this function. It will be fixed in v0.13. Until that you can only import text by
+                    clicking on the ,,Create book" button, then inside the opened book the ,,Add chapter" button.
+                </v-alert>
+            </template>
+            <template v-else>
+                <!--Plain text -->
+                <div class="import-type-button rounded-lg mx-2 mb-4" @click="selectImportType('plain-text')">
+                    <div class="import-type-button-icon-box">
+                        <v-icon large>mdi-text-box</v-icon>
+                    </div>
+                    <span>Plain text</span>
                 </div>
-                <span>Plain text</span>
-            </div>
+                
+                <!-- Text file -->
+                <div class="import-type-button rounded-lg mx-2 mb-4" @click="selectImportType('text-file')">
+                    <div class="import-type-button-icon-box">
+                        <v-icon large>mdi-file-document</v-icon>
+                    </div>
+                    <span>Text file</span>
+                </div>
+
+                <!-- E-book -->
+                <div class="import-type-button rounded-lg mx-2 mb-4" @click="selectImportType('e-book')">
+                    <div class="import-type-button-icon-box">
+                        <v-icon large>mdi-book</v-icon>
+                    </div>
+                    <span>E-book</span>
+                </div>
             
-            <!-- Text file -->
-            <div class="import-type-button rounded-lg mx-2 mb-4" @click="selectImportType('text-file')">
-                <div class="import-type-button-icon-box">
-                    <v-icon large>mdi-file-document</v-icon>
+                <!-- Youtube -->
+                <div class="import-type-button rounded-lg mx-2 mb-4" @click="selectImportType('youtube')">
+                    <div class="import-type-button-icon-box">
+                        <v-icon large>mdi-youtube</v-icon>
+                    </div>
+                    <span>Youtube</span>
                 </div>
-                <span>Text file</span>
-            </div>
 
-            <!-- E-book -->
-            <div class="import-type-button rounded-lg mx-2 mb-4" @click="selectImportType('e-book')">
-                <div class="import-type-button-icon-box">
-                    <v-icon large>mdi-book</v-icon>
+                <!-- Jellyfin subtitle -->
+                <div class="import-type-button rounded-lg mx-2 mb-4" @click="selectImportType('jellyfin-subtitle')">
+                    <div class="import-type-button-icon-box">
+                        <v-icon large>mdi-movie</v-icon>
+                    </div>
+                    <span>Jellyfin subtitle</span>
                 </div>
-                <span>E-book</span>
-            </div>
-        
-            <!-- Youtube -->
-            <div class="import-type-button rounded-lg mx-2 mb-4" @click="selectImportType('youtube')">
-                <div class="import-type-button-icon-box">
-                    <v-icon large>mdi-youtube</v-icon>
-                </div>
-                <span>Youtube</span>
-            </div>
 
-            <!-- Jellyfin subtitle -->
-            <div class="import-type-button rounded-lg mx-2 mb-4" @click="selectImportType('jellyfin-subtitle')">
-                <div class="import-type-button-icon-box">
-                    <v-icon large>mdi-movie</v-icon>
+                <!-- Subtitle file -->
+                <div class="import-type-button rounded-lg mx-2 mb-4" @click="selectImportType('subtitle-file')">
+                    <div class="import-type-button-icon-box">
+                        <v-icon large>mdi-subtitles</v-icon>
+                    </div>
+                    <span>Subtitle file</span>
                 </div>
-                <span>Jellyfin subtitle</span>
-            </div>
 
-            <!-- Subtitle file -->
-            <div class="import-type-button rounded-lg mx-2 mb-4" @click="selectImportType('subtitle-file')">
-                <div class="import-type-button-icon-box">
-                    <v-icon large>mdi-subtitles</v-icon>
+                <!-- Website -->
+                <div class="import-type-button rounded-lg mx-2 mb-4" @click="selectImportType('website')" v-if="websiteImportSupported">
+                    <div class="import-type-button-icon-box">
+                        <v-icon large>mdi-web</v-icon>
+                    </div>
+                    <span>Website</span>
                 </div>
-                <span>Subtitle file</span>
-            </div>
 
-            <!-- Website -->
-            <div class="import-type-button rounded-lg mx-2 mb-4" @click="selectImportType('website')" v-if="websiteImportSupported">
-                <div class="import-type-button-icon-box">
-                    <v-icon large>mdi-web</v-icon>
+                <!-- Website not supported or loading data -->
+                <div class="import-type-button rounded-lg mx-2 mb-4" v-if="!websiteImportSupported">
+                    <div class="import-type-button-icon-box">
+                        <v-icon large v-if="loading">mdi-web</v-icon>
+                        <div class="pt-4" v-if="!loading">Language not supported</div>
+                    </div>
+                    <span>
+                        <!-- Loading -->
+                        <v-progress-circular
+                            v-if="loading"
+                            indeterminate
+                            color="primary"
+                            size="16"
+                            width="2"
+                        ></v-progress-circular>
+
+                        <!-- Not supported label -->
+                        <template v-if="!loading">
+                            Website
+                        </template>
+                    </span>
                 </div>
-                <span>Website</span>
-            </div>
-
-            <!-- Website not supported or loading data -->
-            <div class="import-type-button rounded-lg mx-2 mb-4" v-if="!websiteImportSupported">
-                <div class="import-type-button-icon-box">
-                    <v-icon large v-if="loading">mdi-web</v-icon>
-                    <div class="pt-4" v-if="!loading">Language not supported</div>
-                </div>
-                <span>
-                    <!-- Loading -->
-                    <v-progress-circular
-                        v-if="loading"
-                        indeterminate
-                        color="primary"
-                        size="16"
-                        width="2"
-                    ></v-progress-circular>
-
-                    <!-- Not supported label -->
-                    <template v-if="!loading">
-                        Website
-                    </template>
-                </span>
-            </div>
+            </template>
 
             <!--
             <div class="import-type-button rounded-lg mx-2 mb-4" @click="selectImportType('rss')">
