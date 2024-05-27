3f124518457195aa9138dd1ca57fcd314297728d
simjanos-dev <simjanos.dev@gmail.com>
2024-05-27 11:06:26 +0200

    Controller refactoring 27. (#103)

---
+++ app/Services/DictionaryService.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Dictionary;
+use Illuminate\Support\Facades\DB;
+
+class DictionaryService {
+
+    public function __construct() {
+    }
+
+    public function getDictionaries() {
+        $dictionaries = Dictionary::get();
+
+        foreach ($dictionaries as $dictionary) {
+            if ($dictionary->database_table_name == 'API') {
+                $dictionary->records = '-';
+            } else {
+                $dictionary->records = DB
+                    ::table($dictionary->database_table_name)
+                    ->selectRaw('count(*) as record_count')
+                    ->get();
+
+                $dictionary->records = $dictionary->records[0]->record_count;
+            }
+        }
+
+        return $dictionaries;
+    }
+
+    public function getDictionary($dictionaryId) {
+        $dictionary = Dictionary
+            ::where('id', $dictionaryId)
+            ->first();
+
+        if ($dictionary->database_table_name == 'API') {
+            $dictionary->records = '-';
+        } else {
+            $dictionary->records = DB
+                ::table($dictionary->database_table_name)
+                ->selectRaw('count(*) as record_count')
+                ->get();
+
+            $dictionary->records = $dictionary->records[0]->record_count;
+        }
+
+        return $dictionary;
+    }
+}
