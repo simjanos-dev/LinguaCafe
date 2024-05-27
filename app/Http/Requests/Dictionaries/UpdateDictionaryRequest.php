ac7e29e8af9a1beb3b282140aea0a073f0803eba
simjanos-dev <simjanos.dev@gmail.com>
2024-05-27 11:33:18 +0200

    Controller refactoring 28. (#103)

---
+++ app/Http/Requests/Dictionaries/UpdateDictionaryRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Dictionaries;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateDictionaryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => 'required|numeric',
+            'name' => 'string',
+            'source_language' => 'string',
+            'target_language' => 'string',
+            'color' => 'string',
+            'enabled' => 'boolean',
+        ];
+    }
+}
