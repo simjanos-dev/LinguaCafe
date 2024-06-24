7e7fddf38051e99486cad88bd0d974d133bca9f4
simjanos-dev <simjanos.dev@gmail.com>
2024-06-24 22:28:42 +0200

    Added basic job queue implementation

---
+++ app/Jobs/ProcessChapter.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class ProcessChapter implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    private $message;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        usleep(1000);
+        Log::debug('Test task: ' . $this->message);
+    }
+}
