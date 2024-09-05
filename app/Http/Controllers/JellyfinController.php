8daa1589d56e0a0114aeed0d99b9cca30215fb5e
simjanos-dev <simjanos.dev@gmail.com>
2024-09-05 20:19:29 +0200

    Removed unused functions

--- app/Http/Controllers/JellyfinController.php
+++ app/Http/Controllers/JellyfinController.php
@@ -4,28 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Services\JellyfinService;
 
-// request classes
-use App\Http\Requests\Jellyfin\JellyfinApiCallRequest;
-
 class JellyfinController extends Controller
 {
     private $jellyfinService;
 
     function __construct(JellyfinService $jellyfinService) {
         $this->jellyfinService = $jellyfinService;
-    }
-
-    private function jellyfinApiCall (JellyfinApiCallRequest $request) {
-        $method = $request->post('method');
-        $url = $request->post('url');
-
-        try {
-            $response = $this->jellyfinService->makeRequest($method, $url);
-        } catch(\Exception $e) {
-            abort(500, $e->getMessage());
-        }
-
-        return response()->json($response, 200);
     }
 
     public function getJellyfinCurrentlyPlayedSubtitles () {
