8d7261d304b45d1640596748a7f1b6fe51678f80
simjanos-dev <simjanos.dev@gmail.com>
2025-01-05 17:38:33 +0100

    Fixed migrations for production environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,11 +27,11 @@ done
 
 retry_count=0
 
-while [ $retry_count -lt 40 ] && ! php artisan migrate; do
+while [ $retry_count -lt 40 ] && ! php artisan migrate --force; do
     sleep 15
     retry_count=$((retry_count+1))
 done
 
-php artisan db:seed
+php artisan db:seed --force
 
 exec "$@"
