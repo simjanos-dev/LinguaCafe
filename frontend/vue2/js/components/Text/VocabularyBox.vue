e2c9bbe6792037027aa3d6bf37dfba5f9bd76f2c
Aidan <aidan@nvqna.com>
2026-04-15 22:44:42 +0000

    Add word form selector dropdown to dictionary search field

--- frontend/vue2/js/components/Text/VocabularyBox.vue
+++ frontend/vue2/js/components/Text/VocabularyBox.vue
@@ -212,7 +212,25 @@
                             :value="searchField"
                             @change="searchFieldChanged"
                             @keydown.stop=""
-                        ></v-text-field>
+                        >
+                            <template v-slot:append v-if="word && lemma && word !== lemma">
+                                <v-menu offset-y left v-model="formMenuOpen" :close-on-click="false">
+                                    <template v-slot:activator="{ on, attrs }">
+                                        <v-btn icon x-small v-bind="attrs" v-on="on" title="Select word form" @mouseup.stop="">
+                                            <v-icon small>mdi-chevron-down</v-icon>
+                                        </v-btn>
+                                    </template>
+                                    <v-list dense>
+                                        <v-list-item @click.stop="selectForm(lemma, 'lemma')" @mouseup.stop="">
+                                            <v-list-item-title>{{ lemma }} <span class="grey--text text-caption">(lemma)</span></v-list-item-title>
+                                        </v-list-item>
+                                        <v-list-item @click.stop="selectForm(word, 'exact')" @mouseup.stop="">
+                                            <v-list-item-title>{{ word }} <span class="grey--text text-caption">(exact)</span></v-list-item-title>
+                                        </v-list-item>
+                                    </v-list>
+                                </v-menu>
+                            </template>
+                        </v-text-field>
 
                         <!-- Search box -->
                         <vocabulary-search-box
@@ -486,6 +504,7 @@ export default {
             // ui data
             tab: 0,
             searchField: '',
+            formMenuOpen: false,
             searchResults: [],
             showWordImageEditBoxDialog: false,
         }
@@ -504,10 +523,19 @@ export default {
             this.lemmaReading = this._lemmaReading
             this.phraseReading = this._phraseReading
             this.searchField = this._searchField
+            if (localStorage.getItem('linguacafe_search_form_preference') === 'exact' && this.word && this.word !== this._searchField) {
+                this.searchField = this.word
+            }
             this.exampleSentenceText = this._exampleSentenceText
         },
         textToSpeech() {
             this.$emit('textToSpeech')
+        },
+        selectForm(value, type) {
+            localStorage.setItem('linguacafe_search_form_preference', type)
+            this.formMenuOpen = false
+            this.searchField = value
+            this.searchFieldChanged(value)
         },
         searchFieldChanged(event) {
             if (event === '') {
